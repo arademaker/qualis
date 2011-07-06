7a06bd0b67e1217789361853bb4a5ee548c1b24c
Alexandre Rademaker <arademaker@gmail.com>
2011-07-06 00:14:01 -0300

    nova arrumacao

---
+++ clean.R
@@ -0,0 +1,20 @@
+
+library(R.oo)
+
+names <- dir(pattern = "area-[0-9]{2}.csv")
+areas <- NULL
+for(n in names){
+  area <- read.table(n, sep="|", quote="", stringsAsFactors = FALSE, header=TRUE, comment.char = "")
+  area$area <- area[1,2]
+  area[area$ISSN == "","ISSN"] <- NA
+  area$estrato <- trim(area$estrato)
+
+  lines <- grep("\\(", area$titulo)
+  titulos <- area[lines,3]
+  titulos.t <- gsub("(.*)(\\(.*\\))(.*)","\\1 \\3",titulos)
+  titulos.t <- trim(titulos.t)
+  titulos.o <- gsub("(.*)\\((.*)\\)(.*)","\\2",titulos)
+  area[lines,"titulo"] <- titulos.t
+  area[lines,"obs"] <- titulos.o
+  areas <- rbind(areas,area)
+}
